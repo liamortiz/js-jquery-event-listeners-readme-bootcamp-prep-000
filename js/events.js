d6c72644152a57b19e758962b3981e59ef6fb1df
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-08 07:00:26 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
-//define functions here
+function getIt() {
+  $('p').click(() => {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $(document).load(() => {
+    $('img').addClass("tasty")
+  })
+}
+
+function submitIt() {
+  $('form').submit(() => {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt() {
+  $("#typing").keydown((event) => {
+    if (event.which === 71) {
+      alert("")
+    }
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  frameIt()
+  pressIt()
 });
